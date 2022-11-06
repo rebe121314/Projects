6f1f3b32d8a7fadba80c53f4509ffd5efdaeef0c
rebe121314 <64719929+rebe121314@users.noreply.github.com>
2022-11-06 15:53:10 -0300

    Add files via upload

---
+++ CP91 Project assignmnet 2/text_analyser.py
@@ -0,0 +1,279 @@
+import nltk
+nltk.download([  "names", "stopwords","vader_lexicon","punkt","wordnet",'omw-1.4', 'maxent_ne_chunker','words'])
+from nltk.corpus import stopwords
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+
+"""
+This whole file works as the procesing of text
+"""
+
+#Text analysis
+
+#open text file in read mode
+text_file = open("read.txt", "r", encoding='utf-8')
+ 
+#read whole file to a string
+data = text_file.read()
+ 
+#close file
+text_file.close()
+ 
+
+#Read the data
+text = data
+
+print('Hello! I will help you to understand the logic of your article (should be teh only one in the reas.txt file, in the same folder')
+
+
+#Sentence tokenization
+sentences = nltk.sent_tokenize(text)
+#print(sentences)
+#Print the number of sentences
+print("")
+print("You wrote", len(sentences), 'sentences')
+
+#Word tokenization
+words = nltk.word_tokenize(text)
+#print(words)
+
+#Print the number of words
+print("")
+print("You wrote", len(words), 'words')
+
+print('Thsi means that ypur wrote', len(words)/len(sentences), 'words per sentence on average')
+
+
+
+#determine most common words in each sentence
+def most_common_words(text, n):
+    words = nltk.tokenize.word_tokenize(text)
+    words = [word.lower() for word in words if word.isalpha()]
+    words = [word for word in words if word not in stopwords.words('english')]
+    fdist = nltk.FreqDist(words)
+    return fdist.most_common(n)
+
+print("")
+print('The 5 most common words in your text are:')
+print(most_common_words(text, 5))
+
+"""
+for i in range(len(sentences)):
+    print('Sentence', i+1, ':', sentences[i])
+    print('Most common words:', most_common_words(sentences[i], 5))
+    print()
+"""
+
+
+
+#Remove stopwords
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+filtered_words = [w for w in words if not w in stop_words]
+#print(filtered_words)
+
+
+
+#Stemming
+#Stemming is the process of reducing inflected (or sometimes derived) words to their stem, base or root form—generally a written word form.
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+stemmed_words = [ps.stem(w) for w in filtered_words]
+#print(stemmed_words)
+print("")
+print("Stemmming is the process of reducing inflected (or sometimes derived) words to their stem, base or root form—generally a written word form.")
+print("")
+#Determine the most common frequent stemmed words
+fdist = nltk.FreqDist(stemmed_words)
+print("Your most common stemmed words are:")
+print(fdist.most_common(10))
+
+
+
+#Lemmatization
+#Lemmatization is the process of grouping together the different inflected forms of a word so they can be analysed as a single item.
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(w) for w in filtered_words]
+#print(lemmatized_words)
+
+#Determine the most common frequent lemmatized words
+fdist = nltk.FreqDist(lemmatized_words)
+#print(fdist.most_common(10))
+
+print('')
+print('Lemmatization is the process of grouping together the different inflected forms of a word so they can be analysed as a single item.')
+print('')
+print("Your most common lemmatized words are:")
+print(fdist.most_common(10))
+
+#Part of speech tagging
+#Part of speech tagging is the process of marking up a word in a text as corresponding to a particular part of speech, based on both its definition and its context.
+tagged_words = nltk.pos_tag(lemmatized_words)
+print('')
+#print('Speech tagging is the process of marking up a word in a text as corresponding to a particular part of speech, based on both its definition and its context.')
+#print('')
+#print('Your tagged words are:')
+#print(tagged_words)
+
+#Named entity recognition
+#Named entity recognition is the process of detecting and classifying key information (entities) in text into pre-defined categories such as the names of persons, organizations, locations, expressions of times, quantities, monetary values, percentages, etc.
+namedEnt = nltk.ne_chunk(tagged_words)
+#print(namedEnt)
+#print('')
+#print('Named entity recognition is the process of detecting and classifying key information (entities) in text into pre-defined categories such as the names of persons, organizations, locations, expressions of times, quantities, monetary values, percentages, etc.')
+#print('')
+#print('Your named entities are:')
+#print(namedEnt)
+
+
+
+#Sentiment analysis
+#Sentiment analysis is the process of determining whether a piece of writing is positive, negative or neutral.
+print('')
+print('Sentiment analysis is the process of determining whether a piece of writing is positive, negative or neutral.')
+print('')
+print('The values you can get are compound (meaning the overall sentiment), neg (meaning the negative sentiment), neu (meaning the neutral sentiment) and pos (meaning the positive sentiment).')
+print('The values go from 0 to 1, where 0 is the lowest and 1 is the highest.')
+#print('Your sentiment analysis is for each sentence is:')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sid = SentimentIntensityAnalyzer()
+#for sentence in sentences:
+ #   print(sentence)
+  #  ss = sid.polarity_scores(sentence)
+   # for k in sorted(ss):
+    #    print('{0}: {1}, '.format(k, ss[k]), end='')
+    #print()
+print('')
+print('Your sentiment analysis is for the whole text is:')
+ss = sid.polarity_scores(text)
+for k in sorted(ss):
+    print('{0}: {1}, '.format(k, ss[k]), end='')
+print()
+
+
+#Text summarization
+#Text summarization is the process of shortening a text document with software, in order to create a summary with the major points of the original document.
+print('')
+print('Text summarization is the process of shortening a text document with software, in order to create a summary with the major points of the original document.')
+print('Make sure your text summary makes sese to the ain points you wanted to talk about in your text')
+print('If it dosent make sense reconsider your text and try again')
+print('')
+def read_article(file_name):
+    file = open(file_name,"r",encoding='utf-8')
+    filedata = file.readlines()
+    article = filedata[0].split(". ")
+    sentences = []
+
+    for sentence in article:
+        print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop()
+
+    return sentences
+
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+
+    all_words = list(set(sent1 + sent2))
+
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+
+    return 1 - cosine_distance(vector1, vector2)
+
+def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue 
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+def generate_summary(file_name, top_n=5):
+    """
+    file_name: the path to the text file
+    top_n: the number of sentences the summary should contain
+    """
+
+
+    stop_words = stopwords.words('english')
+    summarize_text = []
+
+    # Step 1 - Read text anc split it
+    sentences =  read_article(file_name)
+
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+    print("Indexes of top ranked_sentence order are ", ranked_sentence)
+    print('')    
+
+    for i in range(top_n):
+      summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+    # Step 5 - Offcourse, output the summarize texr
+    print("Summarize Text: ", ". ".join(summarize_text))
+
+# Let's begin
+generate_summary( "read.txt", 2)
+
+
+print('')
+print('Ohhh, its time to see some plots!')
+print('They shoudl help you to see what is your text trying to say')
+print('')
+print('Your word cloud is:')
+#plot the word cloud
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+wordcloud = WordCloud().generate(text)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+print('')
+print('Your frequency distribution plot is:')
+#plot the frequency distribution withot stopwords
+fdist = nltk.FreqDist(filtered_words)
+for i in fdist:
+    if i in ['et','al','al.','et.','(',')','[',']','{','}','–','—','/','\\','|',"'",'*','&','^','%','$','#','@','!','~','`','\'','\"','<','>','?','.',',',';',':','_','0','1','2','3','4','5','6','7','8','9']:
+        fdist.pop(i)
+fdist.plot(30,cumulative=False)
+plt.show()
+
+
+print('')
+print('Yeiii we are done!')
+print('I hope you enjoyed this program and it helped you to understand your NS assignemnets better, and you get the general logic of your text')
+
+print('')
+
